a685a5f26e4ce54ddbf1fd53e3590500cb1ba3fe
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-14 14:49:26 -0300

    feat: ensure return call bcrypt with correct values

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,16 @@
+import Bcrypt from 'Bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  test('Should call bcrypt with correct values', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(Bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+})
