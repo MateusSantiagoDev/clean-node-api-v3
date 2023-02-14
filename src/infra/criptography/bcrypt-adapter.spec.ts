246c17682e645362e7b093dbafe099b78ad9e833
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-14 15:17:01 -0300

    test: ensure return throw is bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -25,4 +25,11 @@ describe('Bcrypt Adapter', () => {
     const hashed = await sut.encrypt('any_value')
     expect(hashed).toBe('hash')
   })
+
+  test('Should throw is bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn<any, string>(Bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
